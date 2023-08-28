161c9fdf77424effc9aee59220418ebd4cb3189c
Ik-hwan Kim <12kimih@gmail.com>
2023-08-28 14:19:21 +0900

    fix env setup

--- environment.sh
+++ environment.sh
@@ -22,14 +22,14 @@ if [ -z "$name" ]; then
 fi
 
 # initialize conda
-__conda_setup="$('/home/ik/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$($HOME/anaconda3/bin/conda shell.bash hook 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/ik/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/ik/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/ik/anaconda3/bin:$PATH"
+        export PATH="$HOME/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
