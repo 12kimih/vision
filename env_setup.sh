e6c3da8da569717ee0c2c1a41c3418cd896acdd0
Ik-hwan Kim <12kimih@gmail.com>
2024-07-21 14:20:35 +0900

    use miniconda

--- env_setup.sh
+++ env_setup.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
-# change as you wish
+# environment settings
 default_name="vision"
+python_version=3.11
 conda_packages=(
-    "pip jupyter ipykernel ipywidgets autopep8 black"
-    "numpy scipy matplotlib"
-    "pytorch torchvision torchaudio pytorch-cuda -c pytorch -c nvidia"
+    "pip"
 )
-pip_packages=()
+pip_packages=(
+    "jupyter black[jupyter] isort nbqa gpustat python-dotenv tqdm"
+    "numpy scipy matplotlib seaborn pandas pandarallel scikit-learn scikit-learn-intelex scikit-image"
+    "torch torchvision torchaudio"
+)
 
-# prompt
+# read environment name
 echo -e "A new conda environment will be created: $default_name\n"
 echo -e "  - Press ENTER to confirm the environment name"
 echo -e "  - Press CTRL-C to abort the environment setup"
 echo -e "  - Or specify a different environment name\n"
-
-# read environment name
 read -p "[$default_name] >>> " name
 if [ -z "$name" ]; then
     name="$default_name"
 fi
 
 # initialize conda
-__conda_setup="$($HOME/anaconda3/bin/conda shell.bash hook 2> /dev/null)"
+__conda_setup="$($HOME/miniconda3/bin/conda shell.bash hook 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="$HOME/anaconda3/bin:$PATH"
+        export PATH="$HOME/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 
 # create conda environment
-conda create -n "$name" -y
-
-# check if successful
+conda create -n "$name" python=$python_version -y
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     exit $exit_status
@@ -52,6 +51,7 @@ for packages in "${conda_packages[@]}"; do
 done
 
 # install pip packages
+pip install -U pip setuptools wheel
 for packages in "${pip_packages[@]}"; do
-    pip install $packages
+    pip install -U $packages
 done
